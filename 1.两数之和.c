46c3f0cf4d62085045b5d48110a88cb0f3e473b5
Z1J5Y <z1j5y9939@gmail.com>
2021-01-19 14:02:06 +0800

    test

--- 1.两数之和.c
+++ 1.两数之和.c
@@ -62,9 +62,8 @@
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
-int* twoSum(int* nums, int numsSize, int target, int* returnSize)
-{
-    
+int* twoSum(int* nums, int numsSize, int target, int* returnSize){
+
 }
 // @lc code=end
 
